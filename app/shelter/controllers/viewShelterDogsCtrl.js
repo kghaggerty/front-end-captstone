8f113598ea4c8136ea149110fb3071e16dd00810
Kevin Haggerty <kghaggerty@gmail.com>
2017-12-12 18:21:53 -0600

    added view dogs that a shelter has posted in shelter login section

--- app/shelter/controllers/viewShelterDogsCtrl.js
+++ app/shelter/controllers/viewShelterDogsCtrl.js
@@ -1,5 +1,16 @@
-angular 
-.module("AuthApp")
-.controller("viewShelterDogsCtrl", function ($scope, factory, shelterFactory, AuthFactory, $location){
-    
-})
+angular
+    .module("AuthApp")
+    .controller("viewShelterDogsCtrl", function ($scope, factory, userFactory, AuthFactory, $location) {
+        $scope.dogsToDisplay = []
+        //Grab all dogs
+        userFactory.listDogs().then(data => {
+            //Find current user authentication ID
+            let uid = AuthFactory.getUser().uid
+            //Loop through shelter ID array and find all that equal current UID and push it into dogsTodisplay
+            data.forEach(function (shelterId) {
+                if (uid === shelterId.shelteruid) {
+                    $scope.dogsToDisplay.push(shelterId)
+                }
+            })
+        })
+    })
