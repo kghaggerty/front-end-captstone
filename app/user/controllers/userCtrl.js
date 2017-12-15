f077cd2380a2cd65b9a46aba51218167b7b1b336
Kevin Haggerty <kghaggerty@gmail.com>
2017-12-15 14:53:48 -0600

    added functionality so that dogs will disspear upon click of interested or unisterested button, as they should

--- app/user/controllers/userCtrl.js
+++ app/user/controllers/userCtrl.js
@@ -1,59 +1,99 @@
 angular
-.module("AuthApp")
-.controller("userCtrl", function (userFactory, factory, $scope, AuthFactory, $location) {
-    $scope.dog = []
-    $scope.shelter = []
-    let dogData = []
-    $scope.savedDogs = []
-   //Get dogs from database
-    userFactory.listDogs().then(data => {
-        // console.log(data, "THE DATA")
-        $scope.dog = data
-    })
-    //Get shelters to display shelter name with appropriate dog
-    factory.listShelter().then(data => {
-        $scope.shelter = data
-    })
-    //Interested Button on click will save dog to the user's database
-    $scope.InterestedButton = function ($event) {
-        //on click, post dog to "intersted dogs" in user database
-        let theDogId = $event.target.id
-        userFactory.listUsers().then(data => { 
+    .module("AuthApp")
+    .controller("userCtrl", function (userFactory, factory, $scope, AuthFactory, $location) {
+        $scope.dog = []
+        $scope.shelter = []
+        let dogData = []
+        $scope.savedDogs = []
+        let interestedAndNot = []
+        $scope.dogsToDisplay = []
+        let notInterestedDogs;
+        let interestedDogs;
+
+        //Get shelters to display shelter name with appropriate dog
+        factory.listShelter().then(data => {
+            $scope.shelter = data
+        })
+        //Interested Button on click will save dog to the user's database
+        $scope.InterestedButton = function ($event, dogs) {
+            //on click, post dog to "intersted dogs" in user database
+            let theDogId = $event.target.id
+            userFactory.listUsers().then(data => {
+                let theUser = data.filter(function (user) {
+                    return user.uid === AuthFactory.getUser().uid
+                })[0]
+                userFactory.addInterestedDog(theUser, theUser.id, theDogId)
+                let dogToCutOutofArray = $scope.dogsToDisplay.findIndex(dog => {
+                    return dog.name === dogs.name
+                })
+                //console.log(dogToCutOutofArray, "CUT IT")
+                $scope.dogsToDisplay.splice(dogToCutOutofArray, 1)
+                console.log($scope.dogsToDisplay, "THE SPLICE")
+            })
+        }
+
+
+        userFactory.listUsers().then(data => {
             let theUser = data.filter(function (user) {
                 return user.uid === AuthFactory.getUser().uid
             })[0]
-            userFactory.addInterestedDog(theUser, theUser.id, theDogId)
+            if (interestedDogs === null || interestedDogs === undefined) {
+                console.log("get dogs")
+            } else {
+                theUser.interestedDogs.forEach(eachDog => {
+                    interestedAndNot.push(eachDog)
+                })
+            }
         })
-    }
-    // var interested = [1,2,3,4,5,6,7]
-    // var notInterested = [1,2, 9, 11, 12 ]
-    
-    // var allDogs = [1,2,3,4,5,6,7,8,9,10,11,12]
-    
-    // let allInterested = interested.concat(notInterested)
-    // console.log(allInterested)
-    
-    // //YES, but look at dog.uid
-    // let dogsToDisplay = allDogs.filter(function (dog){
-    //     if (allInterested.indexOf(dog) < 0) {
-    //         return dog
-    //     }
-    // })
-    // console.log(dogsToDisplay)
 
-    //Not Interested Button on click will save dog to the user's database
-    $scope.notInterestedButton = function ($event) {
-        //on click, post dog to "not intersted dogs" in user database
-        let theDogId = $event.target.id
-        userFactory.listUsers().then(data => { 
+        userFactory.listUsers().then(data => {
             let theUser = data.filter(function (user) {
                 return user.uid === AuthFactory.getUser().uid
             })[0]
-            userFactory.addNotInterestedDog(theUser, theUser.id, theDogId)
+            if (notInterestedDogs === null || notInterestedDogs === undefined) {
+                console.log("get dogs")
+            } else {
+                theUser.notInterestedDogs.forEach(eachOtherDog => {
+                    interestedAndNot.push(eachOtherDog)
+                })
+            }
+            //console.log(interestedAndNot, "THE FINAL ARRAY")
         })
-    }
 
-    $scope.viewSavedDogs = function () {
-        $location.url('/user/viewSavedDogs')
-    }
-}) 
+        userFactory.listDogs().then(data => {
+            //console.log(data, "DATAAA")
+            let theDogsToDisplay = data.filter(function (dog) {
+                let dogArray = []
+                dogArray.push(dog.id)
+                //console.log(dogArray)
+                if (interestedAndNot.indexOf(dogArray[0]) < 0) {
+                    //console.log(interestedAndNot.indexOf(dogArray) , "THE INDEx OF CONSOLE")
+                    return dog
+                }
+            })
+            $scope.dogsToDisplay = theDogsToDisplay
+            console.log(theDogsToDisplay, "DOGS TO DISPLAY")
+        })
+
+        //Not Interested Button on click will save dog to the user's database
+        $scope.notInterestedButton = function ($event, dogs) {
+            //on click, post dog to "not intersted dogs" in user database
+            let theDogId = $event.target.id
+            userFactory.listUsers().then(data => {
+                let theUser = data.filter(function (user) {
+                    return user.uid === AuthFactory.getUser().uid
+                })[0]
+                userFactory.addNotInterestedDog(theUser, theUser.id, theDogId)
+                let dogToCutOutofArray = $scope.dogsToDisplay.findIndex(dog => {
+                    return dog.name === dogs.name
+                })
+                //console.log(dogToCutOutofArray, "CUT IT")
+                $scope.dogsToDisplay.splice(dogToCutOutofArray, 1)
+                console.log($scope.dogsToDisplay, "THE SPLICE")
+            })
+        }
+
+        $scope.viewSavedDogs = function () {
+            $location.url('/user/viewSavedDogs')
+        }
+    }) 
--- app/user/controllers/viewSavedDogsCtrl.js
+++ app/user/controllers/viewSavedDogsCtrl.js
@@ -1,33 +1,33 @@
 angular
-.module("AuthApp")
-.controller("viewSavedDogsCtrl", function (userFactory, factory, $scope, AuthFactory, $location) {
-    let dogData = []
-    $scope.savedDogs = []
+    .module("AuthApp")
+    .controller("viewSavedDogsCtrl", function (userFactory, factory, $scope, AuthFactory, $location) {
+        let dogData = []
+        $scope.savedDogs = []
 
-    // $scope.viewSavedDogs = function () {
-    //     $location.url('/user/viewSavedDogs')
-        
-    // }
-    userFactory.listUsers().then(data => { 
-        
-        let theUser = data.filter(function (user) {
-            return user.uid === AuthFactory.getUser().uid
-        })[0]
+        // $scope.viewSavedDogs = function () {
+        //     $location.url('/user/viewSavedDogs')
+
+        // }
+        userFactory.listUsers().then(data => {
+
+            let theUser = data.filter(function (user) {
+                return user.uid === AuthFactory.getUser().uid
+            })[0]
             //get all intereted dogs in current user table
-        userFactory.getInterestedDogs(theUser.id).then(dogData => {
-            //get all dogs that shelters have posted 
-            userFactory.printInterestedDogs().then(viewDogData => {
-                //loop over all dogs
-                viewDogData.forEach(function (dog){
-                    //if a dog.id = a value in the dogData array
-                    if (dogData.indexOf(dog.id) >= 0) {
-                        $scope.savedDogs.push(dog)
-                    }
-                    // } else {
+            userFactory.getInterestedDogs(theUser.id).then(dogData => {
+                //get all dogs that shelters have posted 
+                userFactory.printInterestedDogs().then(viewDogData => {
+                    //loop over all dogs
+                    viewDogData.forEach(function (dog) {
+                        //if a dog.id = a value in the dogData array
+                        if (dogData.indexOf(dog.id) >= 0) {
+                            $scope.savedDogs.push(dog)
+                        }
+                        // } else {
                         //     console.log("you have no saved dogs!")
                         // }
                     })
                 })
+            })
         })
-    })
-}) 
+    }) 
