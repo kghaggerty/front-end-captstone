8f85a44f5e1fc4fccfcd4a3795f123e04a587ba9
Kevin Haggerty <kghaggerty@gmail.com>
2017-12-30 13:22:21 -0600

    added home button that works for user and shelter

--- app/auth/controllers/navCtrl.js
+++ app/auth/controllers/navCtrl.js
@@ -1,21 +1,18 @@
 angular.module("AuthApp").controller("navCtrl",
-function ($scope, $location, AuthFactory, factory) {
-    /*
-    Just a pass-through method to the AuthFactory method of the
-    same name.
-    */
-    $scope.isAuthenticated = () => AuthFactory.isAuthenticated();
-
-    // $scope.finder = event => {
-    //     if (event.key === "Enter") {
-    //         const employee = EmployeeFactory.find($scope.searchString)
-    //         $location.url(`/employees/detail/${employee.id}`)
-    //     }
-    // }
-
-    /*
-    Unauthenticate the client.
-    */
-    $scope.logout = () => AuthFactory.logout();
-
-})
+    function ($scope, $location, AuthFactory, factory, userFactory) {
+        $scope.isAuthenticated = () => AuthFactory.isAuthenticated();
+        $scope.logout = () => AuthFactory.logout();
+        $scope.home = function () {
+            userFactory.listUsers().then(data => {
+                let theUser = data.filter(function (user) {
+                    return user.uid === AuthFactory.getUser().uid
+                })[0]
+                if (theUser === undefined || theUser.firstName === null) {
+                    console.log("You are a shelter")
+                    $location.url('/shelter/shelterWelcome')
+                } else {
+                    $location.url('/user/userWelcome')
+                }
+            })
+        }
+    })
